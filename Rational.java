baeab7ab40b2807bedfe5c1751fe2a68e85175d1
Joel Ye <jye5@stuy.edu>
2015-11-18 06:14:57 -0500

    Fixed typo ya fixed, reverted other changes.

--- Rational.java
+++ Rational.java
@@ -8,16 +8,17 @@ public class Rational {
     private int num;
     private int den;
 
-    //default constructor
+	//default constructor
     public Rational(){
-	num = 0;
-	den = 1;
+		num = 0;
+		den = 1;
     }
 	
-    //overload constructor: checks for division by 0
+	//overload constructor: checks for division by 0
     public Rational(int n, int d){
 	if (d == 0){
-	    System.out.print ("MathError: Division by 0.");
+		this();
+		System.out.print ("MathError: Division by 0.");
 	}
 	else {
 	    num = n;
@@ -25,32 +26,31 @@ public class Rational {
 	}
     }
 	
-    //prints rational number with newlines
+	//prints rational number with newlines
     public String toString(){
-	return num +
+		return num +
 	    "\n" + "-" +
 	    "\n" + den;
     }
 	
-    //float approximation of rational number
+	//float approximation of rational number
     public float floatValue(){
-	return (float)(num)/(den); 		//Casting has precedence to division.
+		return (float)(num)/(den); 		//Casting has precedence to division.
     }
 	
-    //multiplication
+	//multiplication
     public void multiply (Rational n){
         num *= n.num;
-	den *= n.den;		//Needn't worry about division by 0, param is also rational.
+		den *= n.den;		//Needn't worry about division by 0, param is also rational.
     }
 	
-    //division
-    public void divide(Rational n){
-	if (n.num == 0){
-	    System.out.println("MathError: Division by 0");
-	    
+	//division
+	public void divide(Rational n){
+		if (n.num == 0){
+			System.out.println("MathError: Division by 0");
+			return;
+		}
+		num *= n.den;
+		den *= n.num;
 	}
-	num *= n.den;
-	den *= n.num;
-    }
-	
 }
