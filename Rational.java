9575731cfaad227412e9cb34a844876af86e89a2
Bayless <bsmithsalzberg@stuy.edu>
2015-11-17 20:09:40 -0500

    did it work?

---
+++ Rational.java
@@ -0,0 +1,16 @@
+public class Rational {
+
+private int numer;
+private int demon;
+
+public Rational(){
+numer = 0;
+demon = 1;
+}
+public Rational(int n, int d){
+if (d == 0){
+numer = 0;
+demon = 1;
+System.out.print ("you can't divide by 0 silly!");
+}
+}
