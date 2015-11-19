c6777496b998b2b8591cd5cc7b94f0d5ca0603dc
Yanni <Yanni@cinder.local>
2015-11-18 19:58:48 -0500

    dealt with negatives

--- Rational.java
+++ Rational.java
@@ -1,31 +1,40 @@
-//Team Neighbors - Joel Ye and Jannie Li
-//APCS1 pd10
-//HW32 -- Do You Even Add, Bro?
-//2015 - 11 - 18
+// Team Neighbors - Joel Ye and Jannie Li
+// APCS1 pd10
+// HW33 -- Do You Even Add, Bro?
+// 2015-11-18
 
 
-//TO DO: Negatives for gcd and Rational class, and commenting
 public class Rational {
 
     private int num;
     private int den;
 
+
+    // CONSTRUCTORS
+    // ===========================================
     //default constructor
     public Rational(){
 	num = 0;
 	den = 1;
     }
 	
-    //overload constructor: checks for division by 0
+    //overloaded constructor: checks for division by 0
     public Rational(int n, int d){
 	this();
-	if (d == 0)
+	if (d == 0) {
 	    System.out.print ("MathError: Division by 0.");
+	}
 	else {
 	    num = n;
 	    den = d;
 	}
     }
+
+
+
+
+    // METHODS
+    // ===========================================
 	
     //prints rational number with newlines
     public String toString(){
@@ -34,17 +43,22 @@ public class Rational {
 	    "\n" + den +
 	    "\n";
     }
-	
+
+    
     //float approximation of rational number
     public float floatValue(){
-	return (float)(num)/(den); 		//Casting has precedence to division.
+	return (float)(num)/(den);
+	//Casting has precedence to division.
     }
-	
+
+    
     //multiplication
     public void multiply (Rational n){
         num *= n.num;
-	den *= n.den;	//Needn't worry about division by 0, param is also rational.
+	den *= n.den;
+	//Needn't worry about division by 0, param is also rational.
     }
+    
 	
     //division
     public void divide(Rational n){
@@ -56,8 +70,22 @@ public class Rational {
 	den *= n.num;
     }
 
+
+    
+    
+    // HW33 ADDITIONS
+    // ===========================================
+
+    // calc gcd of two ints
     public static int gcd(int a, int b){
-        if(a<0 || b<0) return -1;
+	// deal with negatives
+	if (a < 0) {
+	    a *= -1;
+	}
+	if (b < 0) {
+	    a *= -1;
+	}
+	
         while (b != 0) {
             int num = b;
             b = a % b;
@@ -66,16 +94,22 @@ public class Rational {
         return a;
     }
 
+
+    // return gcd of numerator and denominator
     public int gcd(){
 	return gcd(num,den);
     }
 
+
+    // simplify the numerator and denominator of a Rational object
     public static void reduce(Rational n){
 	int gcd = gcd(n.num, n.den);
 	n.num /= gcd;
 	n.den /= gcd;
     }
 
+
+    // add to another Rational object
     public void add(Rational a){
 	num = num * a.den + den * a.num;
 	den *= a.den;
@@ -103,6 +137,7 @@ public class Rational {
     }
 
     public static void main(String[] args){
+	/*
 	Rational a = new Rational(1,2);
 	Rational b = new Rational(1,3);
 	Rational c = new Rational(6,9);
@@ -116,6 +151,11 @@ public class Rational {
 	Rational d = new Rational(2,4);
 	Rational e = new Rational(3,6);
 	System.out.println(d.compareTo(e));
+	*/
+	System.out.println(gcd(3,6));
+	System.out.println(gcd(-3,6));
+
+	
 
     }
 
